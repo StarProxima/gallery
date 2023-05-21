4c9b62575cb5cc9c476edf24ef82e2fbc4b5d7c3
StarProxima <maks.prozorov1@yandex.ru>
2023-05-21 15:17:32 +0300

    Add media and photo repositories

---
+++ lib/features/photo/data/repositories/photo_repository/photo_repository.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../../api/models/pagination_list/pagination_list.dart';
+import 'photo_api.dart';
+
+import '../../../../../api/api_client_provider.dart';
+import '../../../domain/photo_entity/photo_entity.dart';
+import '../../mappers/photo_mapper.dart';
+
+final photoRepositoryProvider = Provider<PhotoRepository>((ref) {
+  return PhotoRepository(
+    api: PhotoApi(ref.watch(apiClientProvider)),
+  );
+});
+
+class PhotoRepository {
+  final PhotoApi _api;
+  final _mapper = PhotoMapper();
+
+  PhotoRepository({
+    required PhotoApi api,
+  }) : _api = api;
+
+  Future<PaginationList<PhotoEntity>> getPhotos({
+    int? page,
+    int? limit,
+  }) async {
+    final listResultDto = await _api.getPhotos(page: page, limit: limit);
+    final listResultEntity = listResultDto.map(_mapper.toEntity);
+    return listResultEntity;
+  }
+
+  Future<PhotoEntity> getPhoto(int id) async {
+    final photoDto = await _api.getPhoto(id);
+    final photoEntity = _mapper.toEntity(photoDto);
+    return photoEntity;
+  }
+}
